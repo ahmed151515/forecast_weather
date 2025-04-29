9a3ab33091cb2ec1f59bb56144d7549d13061498
ahmed151515 <ahmedarafa.200444@gmail.com>
2025-04-30 00:29:40 +0300

    it is done

--- prepossessing_data.sql
+++ prepossessing_data.sql
@@ -1,15 +1,10 @@
 select * from new;
 
- -- delete first row it is null
-begin transaction
-delete from openmeteo_cairo_1940_2025 where id=0
-commit
-rollback
 
 
 -- null check
 SELECT *
-FROM new
+FROM final_new
 WHERE 
     date IS NULL
     OR weather_code IS NULL
@@ -20,12 +15,12 @@ WHERE
     OR wind_gusts_10m_max IS NULL
     OR wind_direction_10m_dominant IS NULL
     OR shortwave_radiation_sum IS NULL
-    OR et0_fao_evapotranspiration IS NULL;
+    OR et0_fao_evapotranspiration IS NULL or weather_status is null or next_day_weather_status is null;
 
 -- delete all null
 begin transaction
 delete
-FROM new
+FROM final_new
 WHERE 
     date IS NULL
     OR weather_code IS NULL
@@ -36,7 +31,7 @@ WHERE
     OR wind_gusts_10m_max IS NULL
     OR wind_direction_10m_dominant IS NULL
     OR shortwave_radiation_sum IS NULL
-    OR et0_fao_evapotranspiration IS NULL;
+    OR et0_fao_evapotranspiration IS NULL or weather_status is null or next_day_weather_status is null;
 commit
 rollback
 -- add weather_status 
@@ -47,30 +42,38 @@ alter table new add weather_status varchar(30)
 UPDATE new
 SET weather_status = CASE
     WHEN weather_code = 0 THEN 'Clear sky'
-    WHEN weather_code = 1 THEN 'Mainly clear'
-    WHEN weather_code = 2 THEN 'Partly cloudy'
-    WHEN weather_code = 3 THEN 'Overcast'
-    WHEN weather_code IN (45, 48) THEN 'Fog'
-    WHEN weather_code IN (51, 53, 55) THEN 'Drizzle'
-    WHEN weather_code IN (56, 57) THEN 'Freezing drizzle'
-    WHEN weather_code IN (61, 63, 65) THEN 'Rain'
-    WHEN weather_code IN (66, 67) THEN 'Freezing rain'
-    WHEN weather_code IN (71, 73, 75) THEN 'Snow fall'
-    WHEN weather_code = 77 THEN 'Snow grains'
-    WHEN weather_code IN (80, 81, 82) THEN 'Rain showers'
-    WHEN weather_code IN (85, 86) THEN 'Snow showers'
-    WHEN weather_code = 95 THEN 'Thunderstorm'
-    WHEN weather_code IN (96, 99) THEN 'Thunderstorm with hail'
+    WHEN weather_code BETWEEN 1 AND 3 THEN 'Cloudy'
+    WHEN weather_code BETWEEN 40 AND 49 THEN 'Fog'
+    WHEN weather_code BETWEEN 50 AND 59 THEN 'Drizzle'
+    WHEN weather_code BETWEEN 60 AND 69 THEN 'Rain'
+    WHEN weather_code BETWEEN 70 AND 79 THEN 'Snow'
+    WHEN weather_code BETWEEN 80 AND 99 THEN 'Storm'
     ELSE 'Unknown'
 END;
 
-select *, LEAD(weather_status)
-into final_data from new;
+select
+id,
+MONTH(date) AS month,
+    DAY(date) AS day,  
+    date,
+    weather_code,
+    temperature_2m_mean,
+    temperature_2m_max,
+    temperature_2m_min,
+    wind_speed_10m_max,
+    wind_gusts_10m_max,
+    wind_direction_10m_dominant,
+    shortwave_radiation_sum,
+    et0_fao_evapotranspiration,
+	weather_status,
+    LEAD(weather_status) OVER (ORDER BY id) AS next_day_weather_status
+into final_new from new;
 
-
+-- delete date 1940-1-1
+begin transaction
+delete from final_new where format(date,'yyyy-M-d') = '1940-1-1'
+commit
+select date from final_new where format(date,'yyyy-M-d') = '1940-1-1'
+select format(date,'yyyy-M-d') from final_new --where format(date,'yyyy-M-d') = '1940-1-1'
 select count(weather_status) as count ,weather_status from new group by  weather_status;
 select count(weather_status)  from new ;
-begin transaction
-delete from new where weather_status in ('Mainly clear','Snow fall','Partly cloudy') 
-commit
-rollback
